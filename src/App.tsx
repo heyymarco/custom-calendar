72f2356508c0b138e2e179f5addfe071f528bb25
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-01-30 00:56:18 +0700

    dev stage-2

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
                 from={new Date('2024-01-01')}
                 to={new Date('2026-12-31')}
                 data={[
-                    { from: new Date('2024-01-01'), to: new Date('2024-07-15'), name: 'Utkarsh FD 1' },
+                    { from: new Date('2024-01-01'), to: new Date('2024-08-15'), name: 'Utkarsh FD 1' },
                     { from: new Date('2024-01-01'), to: new Date('2025-04-30'), name: 'Shriram FD Plan 2' },
                     { from: new Date('2024-05-01'), to: new Date('2025-04-15'), name: 'Bajaj Fanserv FD 1' },
                     { from: new Date('2024-06-01'), to: new Date('2026-06-15'), name: 'Mahindra FD Plan 2' },
--- src/components/CustomCalendar/CustomCalendar.tsx
+++ src/components/CustomCalendar/CustomCalendar.tsx
@@ -6,17 +6,23 @@ const monthLetters = Object.fromEntries(
         [index + 1, new Date(2000, index, 1).toLocaleString('default', { month: 'narrow' })]
     )
 );
-console.log(monthLetters)
+const getMonthDays = (date: Date): number => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const isLeap = ((year % 4) == 0 && ((year % 100) != 0 || (year % 400) == 0));
+    return [31, (isLeap ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+}
 
 export interface Task {
     from : Date
     to   : Date
-    name : string
+    name : React.ReactNode
 }
 export interface CustomCalendarProps {
     from  : Date
     to    : Date
     data ?: Task[]
+    mapPrecision ?: number
 }
 export const CustomCalendar = (props: CustomCalendarProps): JSX.Element => {
     // props:
@@ -24,6 +30,7 @@ export const CustomCalendar = (props: CustomCalendarProps): JSX.Element => {
         from,
         to,
         data = [],
+        mapPrecision = 5,
     } = props;
     
     
@@ -41,7 +48,10 @@ export const CustomCalendar = (props: CustomCalendarProps): JSX.Element => {
     
     // jsx:
     return (
-        <div className={styles.calendar}>
+        <div className={styles.calendar} style={{
+            // @ts-ignore
+            '--mapPrecision': mapPrecision,
+        }}>
             <div className={styles.years}>
                 {new Array(yearsCount).fill(null).map((value, index) =>
                     <div key={index}>
@@ -51,7 +61,7 @@ export const CustomCalendar = (props: CustomCalendarProps): JSX.Element => {
             </div>
             <div className={styles.monthGrid} style={{
                 // @ts-ignore
-                '--monthCount': `${monthsCount}`
+                '--monthCount': monthsCount,
             }}>
                 <div className={styles.monthCells}>
                     {new Array(monthsCount).fill(null).map((value, index) =>
@@ -66,11 +76,37 @@ export const CustomCalendar = (props: CustomCalendarProps): JSX.Element => {
                     )}
                 </div>
                 <div className={styles.monthGraphs}>
-                    {data.map((datum, index) => {
+                    {data.map(({from: localFrom, to: localTo, name}, index) => {
+                        const relativeFromColumn = (
+                            (localFrom.getFullYear() * 12 * mapPrecision) + (localFrom.getMonth() * mapPrecision) + Math.round((localFrom.getDate() - 1) / ( getMonthDays(localFrom) - 1) * mapPrecision)
+                            -
+                            (from.getFullYear()      * 12 * mapPrecision) + (from.getMonth()      * mapPrecision) + Math.round((from.getDate()      - 1) / (getMonthDays(from)       - 1) * mapPrecision)
+                        );
+                        const relativeToColumn = (
+                            (localTo.getFullYear()   * 12 * mapPrecision) + (localTo.getMonth()   * mapPrecision) + Math.round((localTo.getDate()   - 1) / (getMonthDays(localTo)    - 1) * mapPrecision)
+                            -
+                            (from.getFullYear()      * 12 * mapPrecision) + (from.getMonth()      * mapPrecision) + Math.round((from.getDate()      - 1) / (getMonthDays(from)       - 1) * mapPrecision)
+                        );
+                        const relativeToSpan = (
+                            relativeToColumn - relativeFromColumn
+                        )
+                        // const relativeFromMonth = from.getMonth()    - fromMonth;
+                        // const relativeToYear    = to.getFullYear()   - fromYear;
+                        // const relativeToMonth   = to.getFullYear()   - fromMonth;
+                        // const chartFrom         = (relativeFromMonth + (12 * relativeFromYear)) * mapPrecision;
+                        // const chartTo           = (relativeToMonth   + (12 * relativeToYear  )) * mapPrecision;
+                        // const chartSpan         = chartTo - chartFrom;
                         // jsx:
                         return (
                             <div className={styles.monthRow}>
-                                <div className={styles.monthChart}>test</div>
+                                <div className={styles.monthChart} style={{
+                                    // @ts-ignore
+                                    '--chartFrom' : relativeFromColumn + 1,
+                                    // @ts-ignore
+                                    '--chartSpan' : Math.max(relativeToSpan, 1),
+                                }}>
+                                    {name}
+                                </div>
                             </div>
                         );
                     })}
